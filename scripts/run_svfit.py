a610e1a241ac066c69546f4be541e6967326df75
Klitos <kleitos.savva.97@gmail.com>
2024-08-20 07:57:35 +0100

    Updating svfit to run in chunks (Resetting condor queue time to short)

--- scripts/run_svfit.py
+++ scripts/run_svfit.py
@@ -11,7 +11,6 @@ import os
 from alive_progress import alive_bar
 import argparse
 
-
 def calculate_p4(pt: ak.Array, eta: ak.Array, phi: ak.Array, mass: ak.Array) -> ak.Array:
     return ak.zip(
         {
@@ -54,15 +53,22 @@ def process_event(lep1, lep2, mode, kappa, metx, mety, covMatrix,
         return (-1, -1)
 
 
-def run_svfit(input_file: str, channel_: str):
+def run_svfit(input_file: str, channel_: str, chunk_number: int, start_idx: int, end_idx: int):
+    # Load the specific chunk of data from the parquet file
     columns = [
         "met_pt", "met_phi", "met_covXX", "met_covXY", "met_covYY",
         "pt_1", "eta_1", "phi_1", "mass_1", "decayMode_1",
         "pt_2", "eta_2", "phi_2", "mass_2", "decayMode_2",
         "event", "run", "lumi"
     ]
-    events = ak.from_parquet(input_file, columns=columns)
+
+    events = ak.from_parquet(input_file, columns=columns)[start_idx:end_idx]
     nevents = len(events)
+
+    # Create a directory for svfit outputs if it doesn't exist
+    output_dir = os.path.join(os.path.dirname(input_file), "svfit")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     channel = ak.Array([channel_] * nevents)
     dummy = ak.Array([0] * nevents)
@@ -122,7 +128,6 @@ def run_svfit(input_file: str, channel_: str):
         matrix[1][1] = cov[1][1]
         rootMETMatrices.append(matrix)
 
-
     lep_1 = calculate_p4(events.pt_1, events.eta_1, events.phi_1, events.mass_1)
     lep_2 = calculate_p4(events.pt_2, events.eta_2, events.phi_2, events.mass_2)
 
@@ -145,35 +150,29 @@ def run_svfit(input_file: str, channel_: str):
 
     with alive_bar(nevents) as bar:
         for index in range(nevents):
-            try:
-                mass, mass_err = process_event(
-                    lep_1[index], lep_2[index], mode[index], kappa[index],
-                    metx[index], mety[index], rootMETMatrices[index],
-                    leps1_MeasuredTauLepton[index], leps2_MeasuredTauLepton[index],
-                    do_massConstraint, mass_constraint, verbosity
-                )
-                svfitMass_values[index] = mass
-                svfitMass_err_values[index] = mass_err
-            except Exception as exc:
-                print(f"Event {index} generated an exception: {exc}")
-                svfitMass_values[index] = -1
-                svfitMass_err_values[index] = -1
+            mass, mass_err = process_event(
+                lep_1[index], lep_2[index], mode[index], kappa[index],
+                metx[index], mety[index], rootMETMatrices[index],
+                leps1_MeasuredTauLepton[index], leps2_MeasuredTauLepton[index],
+                do_massConstraint, mass_constraint, verbosity
+            )
+            svfitMass_values[index] = mass
+            svfitMass_err_values[index] = mass_err
 
             bar()
 
     columns_to_store = ["event", "run", "lumi"]
-    events = ak.from_parquet(input_file, columns=columns_to_store)
-    events = ak.with_field(events, svfitMass_values, "svfitMass")
-    events = ak.with_field(events, svfitMass_err_values, "svfitMass_err")
+    chunk_events = ak.from_parquet(input_file, columns=columns_to_store)[start_idx:end_idx]
+    chunk_events = ak.with_field(chunk_events, svfitMass_values, "svfitMass")
+    chunk_events = ak.with_field(chunk_events, svfitMass_err_values, "svfitMass_err")
 
-    output_file = input_file.replace("merged", "svfit")
-    ak.to_parquet(events, output_file)
+    output_file = os.path.join(output_dir, f"svfit_chunk{chunk_number}.parquet")
+    ak.to_parquet(chunk_events, output_file)
 
-
-def run_processes(base_dir, use_condor=False):
+def run_processes(base_dir, use_condor=False, chunk_size=10000):
     for channel in os.listdir(base_dir):
         channel_dir = os.path.join(base_dir, channel)
-        if channel != "tt" :
+        if channel != "tt":
             continue
         if os.path.isdir(channel_dir):
             for process in os.listdir(channel_dir):
@@ -183,19 +182,25 @@ def run_processes(base_dir, use_condor=False):
                     if os.path.isdir(variation_dir):
                         parquet_file = os.path.join(variation_dir, 'merged.parquet')
                         if os.path.exists(parquet_file):
-                            run_process(parquet_file, channel, use_condor)
+                            run_process(parquet_file, channel, use_condor, chunk_size)
 
+def run_process(parquet_file, channel, use_condor, chunk_size):
+    events = ak.from_parquet(parquet_file, columns=["event"])  # Load only the event column to determine size
+    nevents = len(events)
+    del events
 
-def run_process(parquet_file, channel, use_condor):
-    if not use_condor:
-        run_svfit(parquet_file, channel)
-    else:
-        shell_script_path = create_shell_script(parquet_file, channel)
-        submission_file_path = create_condor_submission_file(shell_script_path)
-        submit_condor_job(submission_file_path)
+    for chunk_number, start_idx in enumerate(range(0, nevents, chunk_size)):
+        end_idx = min(start_idx + chunk_size, nevents)
 
+        if not use_condor:
+            print(f"Running chunk {chunk_number} for {channel} from {start_idx} to {end_idx}")
+            run_svfit(parquet_file, channel, chunk_number, start_idx, end_idx)
+        else:
+            shell_script_path = create_shell_script(parquet_file, channel, chunk_number, start_idx, end_idx)
+            submission_file_path = create_condor_submission_file(shell_script_path)
+            submit_condor_job(submission_file_path)
 
-def create_shell_script(parquet_file, channel):
+def create_shell_script(parquet_file, channel, chunk_number, start_idx, end_idx):
     script_content = f"""
 #!/bin/bash
 LD_LIBRARY_PATH=/usr/local/lib
@@ -205,9 +210,9 @@ export LIBRARY_PATH=$LIBRARY_PATH:$PWD/TauAnalysis/ClassicSVfit/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/TauAnalysis/ClassicSVfit/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.30.04/x86_64-centosstream9-gcc113-opt/lib/
 
-python3 scripts/run_svfit.py --input_file {parquet_file} --channel {channel} --running_dir
+python3 scripts/run_svfit.py --input_file {parquet_file} --channel {channel} --chunk_number {chunk_number} --start_idx {start_idx} --end_idx {end_idx} --running_dir
 """
-    shell_script_path = os.path.join(os.path.dirname(parquet_file), 'svfit', 'svfit.sh')
+    shell_script_path = os.path.join(os.path.dirname(parquet_file), 'svfit', 'condor', f'svfit_chunk{chunk_number}.sh')
     if not os.path.exists(os.path.dirname(shell_script_path)):
         os.makedirs(os.path.dirname(shell_script_path))
     with open(shell_script_path, 'w') as f:
@@ -221,15 +226,15 @@ def create_condor_submission_file(shell_script_path):
     directory = os.path.dirname(shell_script_path)
     submission_file_content = f"""
 executable = {shell_script_path}
-output = {directory}/svfit.out
-error = {directory}/svfit.err
-log = {directory}/svfit.log
+output = {directory}/svfit_chunk$(Process).out
+error = {directory}/svfit_chunk$(Process).err
+log = {directory}/svfit_chunk$(Process).log
 request_memory = 8G
 getenv = True
-+MaxRuntime = 35000
++MaxRuntime = 10800
 queue
 """
-    submission_file_path = os.path.join(directory, 'condor_submit.sub')
+    submission_file_path = os.path.join(directory, f'condor_submit_{shell_script_path.split("_")[-1].replace(".sh", "")}.sub')
     with open(submission_file_path, 'w') as f:
         f.write(submission_file_content)
 
@@ -239,22 +244,23 @@ queue
 def submit_condor_job(submission_file_path):
     os.system(f"condor_submit {submission_file_path}")
 
-
 def main():
     parser = argparse.ArgumentParser(description="Run SVFit")
     parser.add_argument("--source_dir", type=str, help="Source directory with parquet files e.g. /vols/cms/ks1021/offline/HiggsDNA/IC/output/test/Run3_2022/")
     parser.add_argument("--use_condor", action="store_true", help="Use condor submission")
     parser.add_argument("--input_file", type=str, help="Input file (used by condor submission)")
     parser.add_argument("--channel", type=str, help="Channel (used by condor submission)")
+    parser.add_argument("--chunk_number", type=int, help="Chunk number (used by condor submission)")
+    parser.add_argument("--start_idx", type=int, help="Start index for the chunk (used by condor submission)")
+    parser.add_argument("--end_idx", type=int, help="End index for the chunk (used by condor submission)")
     parser.add_argument("--running_dir", action="store_true", help="Running directory (used by condor submission)")
+    parser.add_argument("--chunk_size", type=int, default=20000, help="Number of events per chunk")
     args = parser.parse_args()
 
     if not args.running_dir:
-        run_processes(args.source_dir, args.use_condor)
+        run_processes(args.source_dir, args.use_condor, args.chunk_size)
     else:
-
-        run_svfit(args.input_file, args.channel)
-
+        run_svfit(args.input_file, args.channel, args.chunk_number, args.start_idx, args.end_idx)
 
 if __name__ == "__main__":
     main()
