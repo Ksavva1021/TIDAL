4c19e387c39e58ecd0369ed2873dc4d210ad597d
lucasrussell01 <lucascharlesrussell@gmail.com>
2024-11-29 14:39:57 +0000

    update options

--- Draw/scripts/run_datacards.py
+++ Draw/scripts/run_datacards.py
@@ -5,24 +5,16 @@ import yaml
 import argparse
 import subprocess
 
-### COMMANDS TO RUN:
-# ```
-# python Draw/scripts/makeDatacards_cpdecay.py --config=Draw/scripts/cpdecay_medium.yaml --vsjet=5 --IPcut=1.25 --Ecut=0.0 --name=IP_1p25_E_0p0
-# -> SPECIFY vsjet time/number and name
-# ```
-
-# ```
-# python Draw/scripts/hadd_cp_datacards.py -i Draw/Run3_Nov20_medium/IP_1p25_E_0p0/*/*/*/*.root -o Draw/Run3_Nov20_medium/IP_1p25_E_0p0/added_Nov20_medium.root
-# -> SPECIFY OUTPUT FOLDER (from NAME + VSJET)
-# ```
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Run combine workflow for CP analysis")
+    parser = argparse.ArgumentParser(description="Run datacard creation for CP analysis")
     parser.add_argument('--vsjet', type=str, help="vsjet cut medium, tight, vtight", required=True)
     parser.add_argument('--IPcut', type=str, help="IP cut", required=True)
     parser.add_argument('--Ecut', type=str, help="Ecut", required=True)
     parser.add_argument('--name', type=str, help="name of the trial", required=True)
+    parser.add_argument('--batch', action='store_true', help="Run in batch mode (datacards only)")
+    parser.add_argument('--stack', action='store_true', help="Run datacard stacking (if datacards done in batch)")
     return parser.parse_args()
 
 def run_command(command):
@@ -41,26 +33,36 @@ def run_command(command):
         print("\n")
         raise
 
-def run_scan(vsjet, name, IPcut, Ecut):
-    config_path = f"Draw/scripts/cpdecay_{vsjet}.yaml"
-    datacard_path = f"Draw/Run3_Nov20_{vsjet}/{name}/*/*/*/*.root"
-    stacked_path = f"Draw/Run3_Nov20_{vsjet}/{name}/added_Nov20_{vsjet}.root"
+def run_scan(vsjet, name, IPcut, Ecut, batch, stack):
+    config_path = f"Draw/scripts/cpdecay_datacards_run2bins.yaml"
+    with open(config_path, 'r') as file:
+        cfg = yaml.safe_load(file)
+    # path to use for stacking input
+    datacard_path = f"{cfg['output_path']}/{vsjet}/{name}/*/*/*/*.root"
+    stacked_path = f"{cfg['output_path']}/{vsjet}/{name}/added_histo_{vsjet}.root"
+    # convert vsjet string to number
     if vsjet == "medium":
         n_vsjet = 5
     elif vsjet == "tight":
         n_vsjet = 6
     elif vsjet == "vtight":
         n_vsjet = 7
-    # CREATE DATACARDS
-    run_command(f"python Draw/scripts/makeDatacards_cpdecay.py --config={config_path} --vsjet={n_vsjet} --IPcut={IPcut} --Ecut={Ecut} --name={name}")
-    # ADD HISTOGRAMS ACROSS CHANNELS
-    run_command(f"python Draw/scripts/hadd_cp_datacards.py -i {datacard_path} -o {stacked_path}")
+    # IF RUNNING ON BATCH ONLY CREATE DATACARDS
+    if batch:
+        run_command(f"python Draw/scripts/makeDatacards_cpdecay.py --config={config_path} --vsjet={n_vsjet} --IPcut={IPcut} --Ecut={Ecut} --name={vsjet}/{name} --batch")
+    # IF RAN ON BATCH THEN STACK AS SEPARATE STEP
+    elif stack:
+        run_command(f"python Draw/scripts/hadd_cp_datacards.py -i {datacard_path} -o {stacked_path}")
+    # IF RUNNING LOCAL THEN CREATE DATACARDS AND STACK
+    else:
+        run_command(f"python Draw/scripts/makeDatacards_cpdecay.py --config={config_path} --vsjet={n_vsjet} --IPcut={IPcut} --Ecut={Ecut} --name={vsjet}/{name}")
+        run_command(f"python Draw/scripts/hadd_cp_datacards.py -i {datacard_path} -o {stacked_path}")
 
 
 def main():
     args = get_args()
     try:
-        run_scan(args.vsjet, args.name, args.IPcut, args.Ecut)
+        run_scan(args.vsjet, args.name, args.IPcut, args.Ecut, args.batch, args.stack)
     except subprocess.CalledProcessError:
         raise RuntimeError("Production failed!")
 
